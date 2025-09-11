fd8f9688bd38afb8696cd5bc8113f0db0f2e3713
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-09-11 01:00:19 -0300

    take on me, teke me ohhh, (areglo2 del punto2 del tp3 eliminar codigos antiguos y ampliar mas nuevos codigos)

--- src/punto2.js
+++ src/punto2.js
@@ -1,9 +1,25 @@
 let mascotas = [];
 
-let nuevaMascota = {
-    nombre:"luna",
-    tipo: "gato",
-    edad: 3,
-    dueñio: "pablo escobar",
-    vacunada: true
+documento.getelementbyid("formulario").addeventlistener("submit", funcion(evento)){
+    evento.preventdefault();
+
+    let nombre=documento.getelementbyid("nombre").valor;
+    let tipo=documento.getelementbyid("tipo").valor;
+    let dueñio=documento.getelementbyid("dueñio").valor;
+    let edad=documento.getelementbyid("edad").valor;
+
+    let mascotas= {nombre, tipo, dueñio, edad};
+    mascotas.push(mascotas);
+
+    let lista= documento.getelementbyid("listamascotas");
+    lista.innerhtml= "";
+
+    mascotas.forEach(m => {
+    let item = document.createElement("li");
+    item.textContent = `${m.nombre} (${m.tipo}), dueño: ${m.duenio}, edad: ${m.edad}`;
+    lista.appendChild(item);
+  });
+
+  document.getelementbyid("dormulario").reset();
 }
+
