18efacab3df5e951fbf59bd5389df3f13bac8aab
Andycap98 <140664285+Andycap98@users.noreply.github.com>
2025-09-12 22:05:53 -0300

    DOM e importar data js

---
+++ Ejercicio02/js/ui.js
@@ -0,0 +1,7 @@
+import { mascotas, eliminarMascota, obtenerEstadisticas } from './data.js';
+
+const listaMascotas = document.getElementById('listaMascotas');
+const totalMascotasSpan = document.getElementById('totalMascotas');
+const vacunadasSpan = document.getElementById('vacunadas');
+const noVacunadasSpan = document.getElementById('noVacunadas');
+
