198e663d0913803891d57570ed4932facc8083ac
Andycap98 <140664285+Andycap98@users.noreply.github.com>
2025-09-12 21:14:56 -0300

    Create dom.js

---
+++ Ejercicio02/js/dom.js
@@ -0,0 +1,5 @@
+export const mascotaForm = document.getElementById('mascotaForm');
+export const listaMascotas = document.getElementById('listaMascotas');
+export const totalMascotasSpan = document.getElementById('totalMascotas');
+export const vacunadasSpan = document.getElementById('vacunadas');
+export const noVacunadasSpan = document.getElementById('noVacunadas');
