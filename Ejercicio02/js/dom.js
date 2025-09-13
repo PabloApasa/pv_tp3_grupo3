21fb544d90bc0d79880a9c2f4d5f950c552ed8e4
Andycap98 <140664285+Andycap98@users.noreply.github.com>
2025-09-12 21:59:14 -0300

    obtener estadisticas mascotas

--- Ejercicio02/js/dom.js
+++
@@ -1,5 +0,0 @@
-export const mascotaForm = document.getElementById('mascotaForm');
-export const listaMascotas = document.getElementById('listaMascotas');
-export const totalMascotasSpan = document.getElementById('totalMascotas');
-export const vacunadasSpan = document.getElementById('vacunadas');
-export const noVacunadasSpan = document.getElementById('noVacunadas');
--- Ejercicio02/js/mascotas.js
+++
@@ -1,4 +0,0 @@
-//en esta seccion cree archivo para gestionar las mascotas
-export const mascotas= [];
-export function agregarMascota (mascota) {mascotas.push(mascota)};
-export function eliminarMascota (index) {mascotas.splice(index,1)};
